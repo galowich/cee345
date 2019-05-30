990d5f4d75635b730da439d9d9df05cd8dd81742
Emily Honda Lemmerman <eglemmerman@gmail.com>
2019-05-30 09:51:24 -0700

    committing joincsv

--- joincsv.R
+++ joincsv.R
@@ -6,9 +6,20 @@ crashedge <- read_csv(file = "GitHub/cee345/CrashEdgeWeights.csv")
 trafficvol <- read_csv(file = "GitHub/cee345/TrafficVolCounts_mod.csv") %>% 
   rename(SegmentID = `Segment ID`)
 
+lionst <- read_csv(file = "GitHub/cee345/LION_Street_Data.csv")
+
+crash_traffic_join <- lionst %>% 
+  full_join(trafficvol, by = "SegmentID") %>%
+  full_join(crashedge, by = "SegmentID")
+
+crash_traffic_join$SegmentID %>% n_distinct()
+
+crashedge %>% group_by(SegmentID) %>% summarise(count = n()) %>% arrange(desc(count))
 
 
-crash_traffic_join <- crashedge %>% full_join(trafficvol, by = "SegmentID")
+crashedge %>% filter(SegmentID ==  79950) %>% View()
 
+
+trafficvol %>% filter(SegmentID == 292830)
 
 write_csv(crash_traffic_join, path = "GitHub/cee345/crash_traffic_join.csv")
